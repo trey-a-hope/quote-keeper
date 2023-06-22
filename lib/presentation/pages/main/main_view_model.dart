0f4a8590982cfb31d9a1daca8d28c37b9ce8550e
Trey Hope <trey.a.hope@gmail.com>
2023-06-22 06:03:03 -0500

    Login Try Catch
    * Placed login logic within try catch to hopefully find the error on page load for physical device.

--- lib/presentation/pages/main/main_view_model.dart
+++ lib/presentation/pages/main/main_view_model.dart
@@ -34,53 +34,57 @@ class _MainViewModel extends GetxController {
     }
     // Proceed to home page.
     else {
-      // Get user document reference.
-      DocumentReference userDocRef = _usersDB.doc(firebaseUser.uid);
+      try {
+        // Get user document reference.
+        DocumentReference userDocRef = _usersDB.doc(firebaseUser.uid);
 
-      // Check if user already exists.
-      bool userExists = (await userDocRef.get()).exists;
+        // Check if user already exists.
+        bool userExists = (await userDocRef.get()).exists;
 
-      // Set UID to get storage.
-      await _getStorage.write('uid', firebaseUser.uid);
+        // Set UID to get storage.
+        await _getStorage.write('uid', firebaseUser.uid);
 
-      // Set app version and build number.
-      PackageInfo packageInfo = await PackageInfo.fromPlatform();
-      _getStorage.write(Globals.appBuildNumber, packageInfo.buildNumber);
-      _getStorage.write(Globals.appVersion, packageInfo.version);
+        // Set app version and build number.
+        PackageInfo packageInfo = await PackageInfo.fromPlatform();
+        _getStorage.write(Globals.appBuildNumber, packageInfo.buildNumber);
+        _getStorage.write(Globals.appVersion, packageInfo.version);
 
-      // // Records a user ID (identifier) that's associated with subsequent fatal and non-fatal reports.
-      // await FirebaseCrashlytics.instance.setUserIdentifier(_firebaseUser.uid);
+        // Records a user ID (identifier) that's associated with subsequent fatal and non-fatal reports.
+        await FirebaseCrashlytics.instance.setUserIdentifier(firebaseUser.uid);
 
-      if (userExists) {
-        // Request permission from user to receive push notifications.
-        if (Platform.isIOS) {
-          _firebaseMessaging.requestPermission();
+        if (userExists) {
+          // Request permission from user to receive push notifications.
+          if (Platform.isIOS) {
+            _firebaseMessaging.requestPermission();
+          }
+
+          // Fetch the fcm token for this device.
+          String? token = await _firebaseMessaging.getToken();
+
+          // Update fcm token for this device in firebase.
+          if (token != null) {
+            userDocRef.update({'fcmToken': token});
+          }
+        } else {
+          // Create user in firebase.
+          UserModel user = UserModel(
+            imgUrl: firebaseUser.photoURL ?? Globals.dummyProfilePhotoUrl,
+            created: DateTime.now().toUtc(),
+            modified: DateTime.now().toUtc(),
+            uid: firebaseUser.uid,
+            username: firebaseUser.displayName ?? firebaseUser.email,
+            email: firebaseUser.email ?? '',
+            bookIDs: [],
+          );
+
+          await _userService.createUser(user: user);
         }
 
-        // Fetch the fcm token for this device.
-        String? token = await _firebaseMessaging.getToken();
-
-        // Update fcm token for this device in firebase.
-        if (token != null) {
-          userDocRef.update({'fcmToken': token});
-        }
-      } else {
-        // Create user in firebase.
-        UserModel user = UserModel(
-          imgUrl: firebaseUser.photoURL ?? Globals.dummyProfilePhotoUrl,
-          created: DateTime.now().toUtc(),
-          modified: DateTime.now().toUtc(),
-          uid: firebaseUser.uid,
-          username: firebaseUser.displayName ?? firebaseUser.email,
-          email: firebaseUser.email ?? '',
-          bookIDs: [],
-        );
-
-        await _userService.createUser(user: user);
+        // Proceed to home page.
+        Get.offAllNamed(Globals.routeDashboard);
+      } catch (e) {
+        throw Exception(e);
       }
-
-      // Proceed to home page.
-      Get.offAllNamed(Globals.routeDashboard);
     }
   }
 
